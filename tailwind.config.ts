4defa9ca5c22af6d37ed35180556a3fbd6cbe051
Onke Yawa <42420810+onkeyawa24@users.noreply.github.com>
2025-05-08 10:39:18 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,7 +23,7 @@ export default {
 				border: 'hsl(var(--border))',
 				input: 'hsl(var(--input))',
 				ring: 'hsl(var(--ring))',
-				background: 'hsl(var(--background))',
+				/* background: 'hsl(var(--background))', */
 				foreground: 'hsl(var(--foreground))',
 				primary: {
 					DEFAULT: 'hsl(var(--primary))',
